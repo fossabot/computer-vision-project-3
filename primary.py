adcc7f9b2a9c8540f01d62565d6eec6e493bc087
Andrew Ingson <aings1@umbc.edu>
2021-04-15 02:12:27 -0400

    Added external library files and started main program

---
+++ primary.py
@@ -0,0 +1,64 @@
+"""
+File: primary.py
+Created by Andrew Ingson (aings1@umbc.edu)
+Date: 4/15/2021
+CMSC 491 Special Topics - Computer Vision
+
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras import datasets
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from tensorflow.keras import optimizers
+from tensorflow.keras import activations
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import cv2
+import os
+import facenet
+import detect_face
+
+
+def main():
+    print("----- Project 3 -----")
+
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[0],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Virtual devices must be set before GPUs have been initialized
+            print(e)
+
+    with tf.Graph().as_default():
+        cap = cv2.VideoCapture(0)
+
+        while (True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Display the resulting frame
+            cv2.imshow('frame', gray)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
+    exit()
