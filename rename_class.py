832b657bf7fb705b356d93e3529bb13b65308199
Chase Carbaugh <chasec1@umbc.edu>
2021-05-09 17:14:52 -0400

    class renaming works

--- rename_class.py
+++ rename_class.py
@@ -1,12 +1,23 @@
 import fnmatch
-import os
+import cv2
 
-def main():
-    dir = "C:\\Users\\chase\\Documents\\CMSC\\CompVision\\computer-vision-project-3\\input\\"
-    for possible_class in os.listdir(dir):
+from config import *
+
+
+def rename_class_dir(inp_dir=input_directory):
+    # TODO: Add comments
+    for possible_class in os.listdir(inp_dir):
         if fnmatch.fnmatch(possible_class, "new_*"):
-            class_name = input("Who is this?")
-
+            combined_class_dir = os.path.join(inp_dir, possible_class)
+            imgs = os.listdir(combined_class_dir)
+            loaded_img = cv2.imread(os.path.join(combined_class_dir, imgs[0]))
+            cv2.imshow("normalized float64", loaded_img)
+            # hold thingy so cv2 doesn't freak out
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            class_name = input("Who is this? ")
+            new_class_dir = os.path.join(inp_dir, class_name)
+            os.rename(combined_class_dir, new_class_dir)
             print(possible_class)
 
 if __name__ == "__main__":
