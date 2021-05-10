3dffc32c8230b30e1bbb8984083b23712d26d86f
Andrew Ingson <aings1@umbc.edu>
2021-05-10 16:49:45 -0400

    Added input dialog for rename_class.py

--- rename_class.py
+++ rename_class.py
@@ -1,10 +1,24 @@
+"""
+File: rename_class.py
+Created by Andrew Ingson & Chase Carbaugh (aings1@umbc.edu & chasec1@umbc.edu)
+Date: 5/9/2021
+CMSC 491 Special Topics - Computer Vision
+
+"""
+
 import fnmatch
+import tkinter as tk
+from tkinter import simpledialog
+
 import cv2
 
 from config import *
 
 
 def rename_class_dir(inp_dir=input_directory):
+    # create framing for the input dialog box
+    prompt_window = tk.Tk()
+    prompt_window.withdraw()
     # iterate over all classes in input folder
     for possible_class in os.listdir(inp_dir):
 
@@ -15,14 +29,14 @@ def rename_class_dir(inp_dir=input_directory):
             loaded_img = cv2.imread(os.path.join(combined_class_dir, imgs[0]))
 
             # Show user first face in unknown class
-            cv2.imshow("WHOMSTVE IS THIS?", loaded_img)
+            cv2.imshow("Current Face", loaded_img)
 
             # hold thingy so cv2 doesn't freak out
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
             # User names class
-            class_name = input("Who is this? ")
+            class_name = simpledialog.askstring(title="Rename Class", prompt="Who is this?")
             new_class_dir = os.path.join(inp_dir, class_name)
 
             # rename unknown class to user input
